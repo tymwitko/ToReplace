3ebe9fe99c82e67649f3411a2e45558b00ccb564
tymwitko <tymon.kobylecki@gmail.com>
2026-08-31 20:52:23 +0200

    add fab for adding new tasks

--- app/src/main/java/com/tymwitko/toreplace/list/ui/TaskListScreen.kt
+++ app/src/main/java/com/tymwitko/toreplace/list/ui/TaskListScreen.kt
@@ -6,15 +6,24 @@ import android.content.Context
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.tymwitko.toreplace.R
 import com.tymwitko.toreplace.common.ui.ErrorScreen
 import com.tymwitko.toreplace.common.ui.PulseAnimation
+import com.tymwitko.toreplace.common.ui.toImageBitmap
 import com.tymwitko.toreplace.list.TaskListViewModel
 import org.koin.androidx.compose.koinViewModel
 
@@ -23,10 +32,12 @@ fun TaskListScreen(
   viewModel: TaskListViewModel = koinViewModel()
 ) {
   val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+  val density = LocalDensity.current
+  val layoutDirection = LocalLayoutDirection.current
   val clipBoardManager =
     LocalContext.current.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
   Box(
-    modifier = Modifier.statusBarsPadding().navigationBarsPadding()
+    modifier = Modifier.statusBarsPadding().navigationBarsPadding().fillMaxSize()
   ) {
     when (val state = uiState) {
       is TaskListUiState.Success -> {
@@ -47,5 +58,19 @@ fun TaskListScreen(
         }
       }
     }
+    FloatingActionButton(
+      modifier = Modifier
+        .padding(10.dp)
+        .navigationBarsPadding()
+        .align(Alignment.BottomEnd),
+      onClick = {
+
+      },
+      content = {
+        painterResource(R.drawable.add)
+          .toImageBitmap(density, layoutDirection)
+          .let { Icon(it, null) }
+      }
+    )
   }
 }
