b7472f67d724d9e402a66bb11b2ba3c7b4bcc25f
tymwitko <tymon.kobylecki@gmail.com>
2026-09-16 22:18:40 +0200

    redesign list item

--- app/src/main/java/com/tymwitko/toreplace/list/ui/TaskListItem.kt
+++ app/src/main/java/com/tymwitko/toreplace/list/ui/TaskListItem.kt
@@ -1,13 +1,26 @@
 package com.tymwitko.toreplace.list.ui
 
 import android.widget.Toast
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalResources
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.tymwitko.toreplace.R
 import com.tymwitko.toreplace.list.CycleType
+import com.tymwitko.toreplace.list.toStringResource
 
 @Composable
 fun TaskListItem(
@@ -17,21 +30,46 @@ fun TaskListItem(
   dueInDays: Int
 ) {
   val ctx = LocalContext.current
-  Row {
+  val res = LocalResources.current
+
+  Row(
+    modifier = Modifier
+      .fillMaxWidth()
+      .padding(12.dp)
+      .border(width = 1.dp, color = Color.DarkGray, shape = RoundedCornerShape(12.dp))
+      .padding(12.dp),
+    horizontalArrangement = Arrangement.SpaceBetween
+  ) {
     Column {
-      Text(taskName)
-      Text("Due in $dueInDays days")
+      Text(
+        text = taskName,
+        color = MaterialTheme.colorScheme.onBackground
+      )
+      Text(
+        text = res.getString(
+          R.string.due_in,
+          dueInDays.toString(),
+          res.getQuantityString(R.plurals.day, dueInDays)
+        ),
+        color = MaterialTheme.colorScheme.onBackground
+      )
     }
     Button(
       onClick = {
         Toast.makeText(
           ctx,
-          "Reminder reset to $cycleLength ${cycleType.name.lowercase()}",
+          res.getString(
+            R.string.reminder_reset,
+            cycleLength.toString(),
+            res.getQuantityString(cycleType.toStringResource(), cycleLength)
+          ),
           Toast.LENGTH_SHORT
         ).show()
       }
     ) {
-      Text("DID IT")
+      Text(
+        text = stringResource(R.string.done).uppercase()
+      )
     }
   }
 }
